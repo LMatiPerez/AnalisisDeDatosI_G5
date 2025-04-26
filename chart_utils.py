82f1fa17d388ddd94213c0f226466db873f0eddb
Nadia Santillán <nadiasantillan@gmail.com>
2025-04-26 15:30:20 -0300

    Gráficos de alcance

---
+++ chart_utils.py
@@ -0,0 +1,22 @@
+def etiquetas_porcentaje_categoria(g):
+    patches = []
+    for i, bars in enumerate(g.containers):
+        for j, bar in enumerate(bars.datavalues):
+            if len(patches) <= j:
+                patches.append([])
+            patches[j].append(bars.datavalues[j])
+
+    for i, bars in enumerate(g.containers):
+        labels = []
+        for j, _ in enumerate(bars.patches):
+            p = round(patches[j][i]*100/sum(patches[j]),0)
+            labels.append(f"{p:.0f}%" if p > 0 else "")
+        g.bar_label(bars, labels=labels)
+
+def etiquetas_porcentaje_total(g):
+    for i, bars in enumerate(g.containers):
+        labels = []
+        s = sum(bars.datavalues)
+        for j, bar in enumerate(bars.datavalues):
+            labels.append(f"{bar*100/s:.0f}%")
+        g.bar_label(bars, labels=labels)
